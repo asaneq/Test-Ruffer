2e216bd65201c91d1e1f86c3c036b4567c23e011
asaneq <108270470+asaneq@users.noreply.github.com>
2022-06-29 17:17:07 +0600

    v_1.1
    первый экран работает , второй переходит но пустой

--- my_app.py
+++ my_app.py
@@ -1,7 +1,7 @@
-#
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
 from instr import *
+from second_win import *
 
 class MainWin(QWidget):
     def __init__(self):
@@ -17,9 +17,25 @@ class MainWin(QWidget):
         self.resize(win_height, win_widht)
  
     def initUI(self):
-        pass
+        self.hello_txt = QLabel(txt_hello) #для текста добро пожаловать
+        self.instruction = QLabel(txt_instruction) #для текста инструкции
+        self.button = QPushButton(txt_next) #для кнопки начать
+        
+        self.VLayout = QVBoxLayout() #для вертикального расположения виджетов
+        '''Добавление в вертикальное расположение виджетов'''
+        self.VLayout.addWidget(self.hello_txt)
+        self.VLayout.addWidget(self.instruction)
+        self.VLayout.addWidget(self.button)
+
+        self.setLayout(self.VLayout) #установить расположение для окна
+
+
     def connects(self):
-        pass
+        self.button.clicked.connect(self.next_click)
+    
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
 
 
 app = QApplication([])

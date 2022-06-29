2f4405ecec39b079310e5de027788c60fdb209aa
asaneq <108270470+asaneq@users.noreply.github.com>
2022-06-29 17:18:06 +0600

    v_1.1
    пока пустой второй экран

--- second_win.py
+++ second_win.py
@@ -1,1 +1,25 @@
-# напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+from instr import *
+
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle('Здоровье') 
+        self.move(win_x, win_y)
+        self.resize(win_height, win_widht)
+ 
+    def initUI(self):
+        pass
+
+    def connects(self):
+        pass
+    
+    def next_click(self):
+        pass

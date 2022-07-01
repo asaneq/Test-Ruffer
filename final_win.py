1811cf95c8da4d86a35fe8653b8f248bd4d86dbd
asaneq <108270470+asaneq@users.noreply.github.com>
2022-07-01 14:30:20 +0300

    v_1.2

--- final_win.py
+++ final_win.py
@@ -1,1 +1,30 @@
-# напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt , QTime , QTimer
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+from instr import *
+
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle(txt_finalwin) 
+        self.move(win_x, win_y)
+        self.resize(win_height, win_widht)
+
+    def initUI(self):
+        self.index = QLabel(txt_index)
+        self.workheart = QLabel(txt_workheart)
+        self.m_line = QVBoxLayout()
+        self.m_line.addWidget(self.index , alignment = Qt.AlignCenter)
+        self.m_line.addWidget(self.workheart , alignment = Qt.AlignCenter)
+        self.setLayout(self.m_line)
+
+    def connects(self):
+        pass
+    
+    def next_click(self):
+        pass

e03e6e92f0eb4419ac636e3b5a0f3752de3e9cc0
asaneq <108270470+asaneq@users.noreply.github.com>
2022-07-01 14:29:05 +0300

    v_1.2

--- my_app.py
+++ my_app.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt , QTime , QTimer
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
 from instr import *
 from second_win import *
@@ -12,7 +12,7 @@ class MainWin(QWidget):
         self.show()
 
     def set_appear(self):
-        self.setWindowTitle('Здоровье') 
+        self.setWindowTitle(txt_title) 
         self.move(win_x, win_y)
         self.resize(win_height, win_widht)
  
